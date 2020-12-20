5846889caf0a747fe1d99f46d8c10b5d39277ee1
Duyhung2810 <Duyhung2810@Duyhung2810s-MacBook-Pro.local>
2020-12-20 22:58:33 +0700

    training

---
+++ java-api-sboot/initial/src/main/java/com/example/actuatorservice/Person.java
@@ -0,0 +1,13 @@
+
+public class Person{
+    private final long IDP;
+    private final String fullName;
+    private final long birthDay;
+
+    public Person(long IDP, String fullName,long birthDay){
+        this.IDP = IDP;
+        this.fullName= fullName;
+        this.birthDay=birthDay;
+    }
+
+}
